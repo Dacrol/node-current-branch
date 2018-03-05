35b71113a1580ad7b95af7748ef5a47c7ba11832
Dacrol <jonas.ahrens@gmail.com>
2018-03-05 20:47:22 +0100

    Add branch function content

--- get-branch.js
+++ get-branch.js
@@ -2,5 +2,14 @@ const fs = require('fs')
 const path = require('path')
 
 const branch = function () {
-
+  const headpath = path.join(process.cwd(), '.git/HEAD')
+  if (!fs.existsSync(headpath)) {
+    console.warn('No HEAD found, aborting.')
+    return null
+  }
+  const headcontent = fs.readFileSync(headpath, 'utf8')
+  const branchname = headcontent.split('/').pop()
+  return branchname
 }
+
+module.exports = branch
